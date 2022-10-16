dd019bb9e3434007b1dccdc5ffb6fc7fc4012a8f
Lukas Mörtman <lukas@mocko.se>
2022-10-16 23:09:14 +0200

    testing astro with svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), image(), svelte()]
+});
